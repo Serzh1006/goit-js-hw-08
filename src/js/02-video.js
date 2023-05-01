a69e85b0768125492dc540ef8703f730ab5a8b0a
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-05-01 15:23:22 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import { save, load } from './localestorage';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const LOCALE__KEY = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const actualTime = load(LOCALE__KEY);
+
+player
+  .setCurrentTime(actualTime)
+  .then(function (seconds) {
+    seconds = actualTime;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        alert('Время было меньше 0 или больше, чем продолжительность видео');
+        break;
+
+      default:
+        alert('Что-то не так');
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    save(LOCALE__KEY, data.seconds);
+  }, 1000)
+);
