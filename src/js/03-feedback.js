3e575b0955f0f15aa663e0e6dd8e6fcc2a726547
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-05-01 21:31:26 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import { save, load } from './localestorage';
+const throttle = require('lodash.throttle');
+const LOCALE__KEY = 'feedback-form-state';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailEl: document.querySelector('[name = "email"]'),
+  messageEl: document.querySelector('[name = "message"]'),
+};
+
+const dataUser = {};
+
+refs.form.addEventListener('submit', onClickSubForm);
+
+// Проверка localeStorage на наличие данных
+function getDataFromStorage() {
+  const objData = load(LOCALE__KEY);
+  if (objData) {
+    refs.emailEl.value = objData.email;
+    refs.messageEl.value = objData.message;
+  }
+}
+
+getDataFromStorage();
+
+//  Считываем данные полей в localestorage
+
+refs.form.addEventListener(
+  'input',
+  throttle(e => {
+    dataUser[e.target.name] = e.target.value;
+    save(LOCALE__KEY, dataUser);
+  }, 500)
+);
+
+// Submit Form
+
+function onClickSubForm(e) {
+  if (refs.emailEl.value === '' || refs.messageEl.value === '') {
+    alert('Заполните все поля');
+  } else {
+    e.preventDefault();
+    const objForm = {};
+    new FormData(e.currentTarget).forEach((value, index) => {
+      objForm[index] = value;
+    });
+    console.log(objForm);
+    localStorage.removeItem(LOCALE__KEY);
+    e.target.reset();
+  }
+}
