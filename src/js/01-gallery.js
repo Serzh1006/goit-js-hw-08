485b5048a1e03d364a546edf7ab976df141311bb
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-05-01 12:30:53 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+// 1. Создание и добавление изображений в список
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createListImages(galleryItems) {
+  return galleryItems
+    .map((obj) => {
+      const itemList = `<li class="gallery__item"><a class="gallery__link" href="${obj.original}">
+			<img class="gallery__image" src="${obj.preview}" alt="${obj.description}" /></a></li>`;
+      return itemList;
+    })
+    .join("");
+}
+const listImages = createListImages(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", listImages);
+
+// 2. Открытие галереи изображений по клику
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
